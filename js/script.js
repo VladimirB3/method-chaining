e26fcffa9a5c8be936da58073a1e5db29699bffa
Vladimirs Boreckis <v.boreckis@gmail.com>
2020-02-10 13:04:12 +0000

    Method chaining challenge 1 complete

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
+    
+    $("#button1").mouseenter(function() {
+        $("#button1").removeClass("makeRed").addClass("makeBorder");
+    });
+
+    $("#button1").mouseleave(function() {
+        $("#button1").removeClass("makeBorder").addClass("makeRed");
+    });
+    
  
+    
+
 
 
 
