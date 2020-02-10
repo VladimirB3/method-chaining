5bd0443475378e5d7344f85eecffb63776d8fa1e
Vladimirs Boreckis <v.boreckis@gmail.com>
2020-02-10 13:38:10 +0000

    Challenges 3 and 4 of Method Chaining completed

--- js/script.js
+++ js/script.js
@@ -7,11 +7,14 @@ $(document).ready(function() {
     $("#button1").mouseleave(function() {
         $("#button1").removeClass("makeBorder").addClass("makeRed");
     });
+
+    $("#button1").click(function() {
+        $("#para1").hide(1000).show(1000);
+    });
     
- 
+    $("#button2").click(function() {
+        $("#para2").fadeIn(1000).fadeOut(1000);
+    });
     
-
-
-
 
 }); 
